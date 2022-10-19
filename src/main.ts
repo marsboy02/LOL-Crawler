198ec3eaf5f360f657bf156a12a38d5914eef061
Marsboy02 <marsboy0619@gmail.com>
2022-10-19 15:03:47 +0900

    feat: swagger docs 생성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('league of legend Pro Match Statics')
+    .setDescription('리그 오브 레전드 프로 경기의 결과를 확인해주는 API입니다.')
+    .setVersion('0.1')
+    .addTag('LOL')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
