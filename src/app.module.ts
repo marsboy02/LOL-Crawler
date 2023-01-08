02d157a5f256a84dc5c6ffead465a706e9fc6ea8
Marsboy02 <marsboy0619@gmail.com>
2023-01-08 20:36:26 +0900

    feat: postgres DB와 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './infrastructure/health/health.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { dataSourceConfig } from './data-source';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ConfigModule.forRoot(), HealthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    HealthModule,
+    TypeOrmModule.forRoot(dataSourceConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/data-source.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+dotenv.config();
+
+export const dataSourceConfig: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 5432,
+  database: process.env.DB_DATABASE || 'match_data',
+  username: process.env.DB_USERNAME || 'match_data',
+  password: process.env.DB_PASSWORD || 'match_data',
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/**/*.js'],
+  namingStrategy: new SnakeNamingStrategy(),
+  synchronize: false,
+  migrationsRun: true,
+};
+
+export const dataSource = new DataSource(dataSourceConfig);
